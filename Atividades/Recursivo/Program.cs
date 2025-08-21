2ec114299901f88f7aa0010c0bfb789950773938
Higor-Kleemann <higorkleemannoficial@gmail.com>
2025-08-20 22:08:29 -0300

    Add files via upload

---
+++ Atividades/Recursivo/Program.cs
@@ -0,0 +1,33 @@
+﻿static void Main(string[] args)
+    {
+        int x = 3;
+        fun2(x);
+        Console.ReadKey();
+    }
+
+    static void fun2(int n)
+    {
+        if (n > 0)
+        {
+            fun2(n - 1);
+            Console.Write($"{n} ");
+        }
+    }
+
+    {
+    int x = 5;
+    Console.WriteLine($"O Fatorial de {x} é {factorial(x)}");
+    Console.ReadKey();
+    }
+
+static int factorial(int number)
+    {
+        if (number == 1)
+        {
+            return (1);
+        }
+        else
+        {
+            return(number*factorial(number - 1)); 
+        }
+    }
