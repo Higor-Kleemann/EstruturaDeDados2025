a12f539785b981c2f26d88bf27e82f6527cc157a
Higor-Kleemann <higorkleemannoficial@gmail.com>
2025-09-03 20:03:51 -0300

    Add files via upload

---
+++ Atividades/Queue/Program.cs
@@ -0,0 +1,53 @@
+﻿static void Main(string[] args)
+{
+    CallCenter center = new CallCenter();
+    Parallel.Invoke(
+        () => CallersAction(center),
+        () => ConsultantAction(center, "Marcin", ConsoleColor.Red),
+        () => ConsultantAction(center, "James", ConsoleColor.Yellow),
+        () => ConsultantAction(center, "Olivia", ConsoleColor.Green)
+        );
+}
+
+static void CallersAction(CallCenter center)
+{
+    Random random = new Random();
+    while (true)
+    {
+        int clientId = random.Next(1, 10000);
+        int waitingCount = center.Call(clientId);
+        Log($"Incoming call from {clientId}, waiting in the queue: { waitingCount} ");
+        Thread.Sleep(random.Next(1000, 5000));
+    }
+}
+
+static void ConsultantAction(CallCenter center,
+ string name, ConsoleColor color)
+{
+    Random random = new Random();
+    while (true)
+    {
+        IncomingCall call = center.Answer(name);
+        if (call != null)
+        {
+            Console.ForegroundColor = color;
+            Log($"Call #{call.Id} from {call.ClientId} is answered by { call.Consultant}.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Thread.Sleep(random.Next(1000, 10000));
+            center.End(call);
+            Console.ForegroundColor = color;
+            Log($"Call #{call.Id} from {call.ClientId} is ended by { call.Consultant}.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Thread.Sleep(random.Next(500, 1000));
+        }
+        else
+        {
+            Thread.Sleep(100);
+        }
+    }
+}
+
+static void Log(string text)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} { text} ");
+    }
