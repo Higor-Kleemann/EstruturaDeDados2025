864544a45fc9ce695c82f5d5bcafb74cfa68e0ef
Higor-Kleemann <higorkleemannoficial@gmail.com>
2025-09-03 21:39:50 -0300

    Add files via upload

---
+++ Atividades/Stacks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+Console.WriteLine("Invertendo palavras com pilhas (Stacks)");
+Console.WriteLine("Escreva uma palavra: ");
+string palavra = Console.ReadLine();
+Stack<char> chars = new Stack<char>();
+
+foreach (var c in palavra)
+{
+    chars.Push(c);
+}
+
+string palavra_Invertida = string.Empty;
+
+while (chars.Count > 0)
+{
+    char c = chars.Pop();
+    palavra_Invertida += c;
+}
+
+if (palavra == palavra_Invertida)
+{
+    Console.WriteLine($"A palavra {palavra} é um palíndromo");
+}
+else
+{
+    Console.WriteLine($"A palavra {palavra} não é um palíndromo");
+}
