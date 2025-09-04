ca02860f92e2e88780f9d31928a0b6d13399fd14
Higor-Kleemann <higorkleemannoficial@gmail.com>
2025-09-03 21:40:26 -0300

    Add files via upload

---
+++ Palindromo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+Console.WriteLine("Identificando um Palíndromo");
+Console.WriteLine("Digite uma palavra: ");
+string ?palavra = Console.ReadLine().ToLower();
+
+char[] array = palavra!.ToCharArray();
+
+Array.Reverse(array);
+string palavraInvertida = new string(array);
+if (palavra == palavraInvertida)
+{
+    Console.WriteLine("A palavra é um palíndromo.");
+}
+else
+{
+    Console.WriteLine("A palavra não é um palíndromo.");
+}
+Console.ReadLine();
+
+
+
+
