3a1ea5ca6eb1e913b2d3cc772306c728858fa863
Higor Afonso Kleemann <higorkleemannoficial@gmail.com>
2025-10-15 21:54:09 -0300

    Add files via upload

---
+++ Atividades/SelectionSort/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] integerValues = { -11, 12, 42, 0, 1, 90, 68, 6, -9 };
+SelectionSort.SelectionSort.Sort(integerValues);
+Console.WriteLine("Vetor Inteiros");
+Console.WriteLine(string.Join(" , ", integerValues));
+Console.WriteLine("*****************************************");
