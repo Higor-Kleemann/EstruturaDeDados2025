cc972e37b891f631bd40823547c2e1cc53304d32
Higor Afonso Kleemann <higorkleemannoficial@gmail.com>
2025-11-12 20:05:07 -0300

    Add files via upload

---
+++ Atividades/PrograminhaDeBusca/Search/Program.cs
@@ -0,0 +1,20 @@
+﻿int [] numeros = new int [] {1,52,34,56,7,2,3,8,9,10};
+int numero=0;
+bool encontrado = false;
+
+Console.WriteLine("Informe um número para ser procurado no vetor: ");
+numero = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < numeros.Length; i++)
+{
+    if(numeros[i] == numero)
+    {
+        Console.WriteLine($"Número encontrado na posição {i + 1}");
+        encontrado = true;
+    }
+}
+
+if(!encontrado)
+{
+    Console.WriteLine("O número não foi encontrado.");
+}
